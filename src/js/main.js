0b09b3641524d56cc66ba26521947d17af70fc1f
mac <mac@host-192-168-0-202.sohonet.ua>
2022-12-30 13:49:13 +0200

    forms and calc

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import modals from './modules/modals';
 import sliders from './modules/sliders';
 import showMoreStyles from './modules/showMoreStyles';
+import forms from './modules/forms';
+import calc from './modules/calc';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -8,5 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
     modals();
     sliders('.main-slider-item', 'vertical');
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+    forms();
     showMoreStyles('.button-styles', '.styles-2');
+    calc('#size', '#material', '#options', '.promocode', '.calc-price');
 });
